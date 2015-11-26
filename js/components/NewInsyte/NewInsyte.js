0b1717b3217f5031266993c99dc4878ddb4adaad
Marc-André Giroux <mgiroux0@gmail.com>
2015-11-26 08:41:02 -0500

    create insyte

--- js/components/NewInsyte/NewInsyte.js
+++ js/components/NewInsyte/NewInsyte.js
@@ -9,6 +9,7 @@ import styles from './NewInsyte.css';
 import Button from '../Shared/Buttons/Button';
 import SubmitButton from '../Shared/Buttons/SubmitButton';
 import AddInsyteMutation from '../../mutations/AddInsyteMutation';
+import Spinner from '../Shared/Spinner/Spinner';
 
 class AddInsyte extends React.Component {
   constructor(props) {
@@ -16,6 +17,7 @@ class AddInsyte extends React.Component {
       this.state = {
         message: null,
         file: null,
+        loading: false,
       };
   }
 
@@ -25,15 +27,18 @@ class AddInsyte extends React.Component {
     let description = this.refs.description.value;
 
     var onSuccess = (response) => {
+      this.setState({loading: false});
       console.log(response);
-      console.log('Mutation successful!');
       let insyte_id = response.addInsyte.newInsyteEdge.rails_id;
       window.location.href = `#/insyte/${insyte_id}`;
     };
     var onFailure = (transaction) => {
+      this.setState({loading: false});
       var error = transaction.getError() || new Error('Mutation failed.');
       console.error(error);
     };
+
+    this.setState({loading: true});
 
     Relay.Store.update(new AddInsyteMutation({
       title: title,
@@ -51,8 +56,16 @@ class AddInsyte extends React.Component {
   }
 
   render() {
+    let button = this.state.loading ? <div><p>Uploading video, this might take a few minutes.</p><Spinner/></div> :
+                 <SubmitButton onSubmitFunc={this.onInsyteSubmit.bind(this)}
+                               text="Submit New Insyte"/>
+    let dropzoneContent = this.state.file ? this.state.file.name :
+      <div>Click or drop a file here to upload your media</div>
+
     return (
+      <div>
       <div className={'insyto-container insyto-container--large-padding'}>
+        <h2 className="insyto-heading">Create Insyte</h2>
         <form>
         <p className={'insyto-container__message'}>{this.state.message}</p>
         <div className={'form-field-container'}>
@@ -65,13 +78,14 @@ class AddInsyte extends React.Component {
           <Dropzone onDrop={this.onDrop.bind(this)}
                     className={styles['dropzone']}
                     activeClassName={styles['dropzone--active']}>
-            <div> Drop your video file right here to upload it </div>
+            <div>{ dropzoneContent }</div>
           </Dropzone>
         </div>
         <div className={'form-field-container form-field-container--submit'}>
-          <SubmitButton onSubmitFunc={this.onInsyteSubmit.bind(this)} text="Submit New Insyte"/>
+          { button }
         </div>
         </form>
+      </div>
       </div>
     );
   }
